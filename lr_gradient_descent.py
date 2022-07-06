f5ba2993f412f10eae30365cbff1ec2a33709f49
tssup <kwakutenace@gmail.com>
2022-07-06 04:48:19 -0500

    changed epoch

--- lr_gradient_descent.py
+++ lr_gradient_descent.py
@@ -1,7 +1,7 @@
 import numpy as np 
 
 
-def gradient_desc(Y, X, lr = 0.00001, epoch = 69000):
+def gradient_desc(Y, X, lr = 0.00001, epoch = 100):
     W, B = np.random.rand(), np.random.rand()
     N = Y.shape[0]
 
