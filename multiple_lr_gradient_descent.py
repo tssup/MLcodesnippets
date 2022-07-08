88d1d669e031c1027156d0e18631f24060ac781e
tssup <kwakutenace@gmail.com>
2022-07-07 22:28:36 -0500

    revised code - multiple lr grad descent

--- multiple_lr_gradient_descent.py
+++ multiple_lr_gradient_descent.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def gradient_descent(Y, X, lr, epochs):
+def gradient_descent(Y, X, lr = 0.010, epochs = 10000):
     R, N = X.shape
     W = np.random.rand(N, 1)
     B = np.random.rand()
@@ -13,13 +13,13 @@ def gradient_descent(Y, X, lr, epochs):
         print(f'error: {C} \n')
         
         for i in range(W.shape[0]):
-            #grab each column of X for backprop of Weights and reshape for matmul with error
+            #grab each column of X for backprop of weights and reshape for matmul with the error
             col_X = X[:,i].reshape(R, 1)
             #change in error w.r.t. weights
             dC_dW = (2/N) * np.sum(col_X * (Y_hat - Y))
             
             #update weights
-            W[i,0] = W[i, 0] - lr * dC_dW
+            W[i,0] -= lr * dC_dW
        
         #change in error w.r.t bias
         dC_dB = (2/N) * np.sum(Y_hat - Y)
@@ -29,12 +29,7 @@ def gradient_descent(Y, X, lr, epochs):
 
     return (W, B)
                 
-
-    
-
-
-
 if __name__ == '__main__':
-    X = np.random.uniform(10, 1000, (300,5))
-    Y = np.random.rand(300,1)
-    print(feed_forward(Y, X, 0.000000005, 40000))
+    x = np.random.rand(10,1)
+    y = 17 * x - 12
+    print(gradient_descent(y, x))
